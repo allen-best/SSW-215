c55634e4a44b3c85b4a6386414e023e7dad80c10
Liam Brew <33580926+Liam-Brew@users.noreply.github.com>
2019-03-28 14:41:25 -0400

    take 2

--- scan_networks.py
+++ scan_networks.py
@@ -3,24 +3,24 @@ import csv
 import os
 import time
 
-def bash(self, command):
-        """This function runs the input command in the Bash terminal."""
-        subprocess.call("{}".format(command), shell=True)
+def bash(command):
+    """This function runs the input command in the Bash terminal."""
+    subprocess.call("{}".format(command), shell=True)
 
-def scan_networks(self):
+def scan_networks():
     """This method scans for WiFi networks and outputs them as a .csv file to the desktop. To stop scanning the user must enter the keyboard interrupt."""
-    self.bash("echo Resetting Device")
+    bash("echo Resetting Device")
     time.sleep(1)
-    self.bash("airmon-ng check kill")
-    self.bash("echo Enabling Monitor Mode")
+    bash("airmon-ng check kill")
+    bash("echo Enabling Monitor Mode")
     time.sleep(1)
-    self.bash("airmon-ng start wlan0")
-    self.bash("echo Conducting Network Scan")
+    bash("airmon-ng start wlan0")
+    bash("echo Conducting Network Scan")
     time.sleep(1)
-    self.bash("airodump-ng start wlan0mon")
+    bash("airodump-ng start wlan0mon")
     try:
-        self.bash("airodump-ng wlan0 -w output --output-format csv")
+        bash("airodump-ng wlan0 -w output --output-format csv")
     except KeyboardInterrupt:
-        self.bash("killall airodump-ng")
+        bash("killall airodump-ng")
 
 scan_networks()
