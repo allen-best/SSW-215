fec604f5332e7bd0e56dda5f921b9c12ae2aea36
Liam Brew <33580926+Liam-Brew@users.noreply.github.com>
2019-03-28 14:43:34 -0400

    Add files via upload

--- scan_networks.py
+++ scan_networks.py
@@ -10,16 +10,16 @@ def bash(command):
 def scan_networks():
     """This method scans for WiFi networks and outputs them as a .csv file to the desktop. To stop scanning the user must enter the keyboard interrupt."""
     bash("echo Resetting Device")
-    time.sleep(1)
+    time.sleep(3)
     bash("airmon-ng check kill")
     bash("echo Enabling Monitor Mode")
-    time.sleep(1)
+    time.sleep(3)
     bash("airmon-ng start wlan0")
     bash("echo Conducting Network Scan")
-    time.sleep(1)
+    time.sleep(3)
     bash("airodump-ng start wlan0mon")
     try:
-        bash("airodump-ng wlan0 -w output --output-format csv")
+        bash("airodump-ng wlan0mon -w output --output-format csv")
     except KeyboardInterrupt:
         bash("killall airodump-ng")
 
